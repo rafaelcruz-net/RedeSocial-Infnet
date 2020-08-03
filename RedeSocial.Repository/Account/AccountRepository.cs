68b92107c3a019462a5a5b3f095550b5588dea47
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2020-08-03 10:04:28 -0300

    Configuração do Repositorios

--- RedeSocial.Repository/Account/AccountRepository.cs
+++ RedeSocial.Repository/Account/AccountRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using RedeSocial.Domain.Account;
 using RedeSocial.Domain.Account.Repository;
+using RedeSocial.Repository.Context;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,38 +15,49 @@ namespace RedeSocial.Repository.Account
     public class AccountRepository : IUserStore<Domain.Account.Account>, IAccountRepository
     {
         private bool disposedValue;
-        private bool disposedValue1;
 
-        public Task<IdentityResult> CreateAsync(Domain.Account.Account user, CancellationToken cancellationToken)
+        private RedeSocialContext Context { get; set; }
+
+        public AccountRepository(RedeSocialContext redeSocialContext)
         {
-            throw new NotImplementedException();
+            this.Context = redeSocialContext;
         }
 
-        public Task<IdentityResult> DeleteAsync(Domain.Account.Account user, CancellationToken cancellationToken)
+
+        public async Task<IdentityResult> CreateAsync(Domain.Account.Account user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            this.Context.Accounts.Add(user);
+            await this.Context.SaveChangesAsync();
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(Domain.Account.Account user, CancellationToken cancellationToken)
+        {
+            this.Context.Accounts.Remove(user);
+            await this.Context.SaveChangesAsync();
+            return IdentityResult.Success;
         }
 
        
 
         public Task<Domain.Account.Account> FindByIdAsync(string userId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return this.Context.Accounts.FirstOrDefaultAsync(x => x.Id == new Guid(userId));
         }
 
         public Task<Domain.Account.Account> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return this.Context.Accounts.FirstOrDefaultAsync(x => x.Name == normalizedUserName);
         }
 
         public Task<string> GetNormalizedUserNameAsync(Domain.Account.Account user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(user.Name);
         }
 
         public Task<string> GetUserIdAsync(Domain.Account.Account user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(user.Id.ToString());
         }
 
         public Task<string> GetUserNameAsync(Domain.Account.Account user, CancellationToken cancellationToken)
@@ -61,14 +75,23 @@ namespace RedeSocial.Repository.Account
             throw new NotImplementedException();
         }
 
-        public Task<IdentityResult> UpdateAsync(Domain.Account.Account user, CancellationToken cancellationToken)
+        public async Task<IdentityResult> UpdateAsync(Domain.Account.Account user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var accountToUpdate = await this.Context.Accounts.AsNoTracking().FirstOrDefaultAsync(x => x.Id == user.Id);
+            
+            accountToUpdate = user;
+            this.Context.Entry(accountToUpdate).State = EntityState.Modified;
+
+            this.Context.Accounts.Add(accountToUpdate);
+            await this.Context.SaveChangesAsync();
+
+            return IdentityResult.Success;
+
         }
 
         protected virtual void Dispose(bool disposing)
         {
-            if (!disposedValue1)
+            if (!disposedValue)
             {
                 if (disposing)
                 {
@@ -77,7 +100,7 @@ namespace RedeSocial.Repository.Account
 
                 // TODO: free unmanaged resources (unmanaged objects) and override finalizer
                 // TODO: set large fields to null
-                disposedValue1 = true;
+                disposedValue = true;
             }
         }
 
--- RedeSocial.Repository/Account/ProfileRepository.cs
+++ RedeSocial.Repository/Account/ProfileRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using RedeSocial.Domain.Account;
+using RedeSocial.Repository.Context;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,59 +12,109 @@ namespace RedeSocial.Repository.Account
 {
     public class ProfileRepository : IRoleStore<Profile>
     {
-        public Task<IdentityResult> CreateAsync(Profile role, CancellationToken cancellationToken)
+        private bool disposedValue;
+
+        private RedeSocialContext Context { get; set; }
+
+        public ProfileRepository(RedeSocialContext redeSocialContext)
         {
-            throw new NotImplementedException();
+            this.Context = redeSocialContext;
         }
 
-        public Task<IdentityResult> DeleteAsync(Profile role, CancellationToken cancellationToken)
+        public async Task<IdentityResult> CreateAsync(Profile role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            this.Context.Profiles.Add(role);
+            await this.Context.SaveChangesAsync();
+            return IdentityResult.Success;
         }
 
-        public void Dispose()
+        public async Task<IdentityResult> DeleteAsync(Profile role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            this.Context.Profiles.Remove(role);
+            await this.Context.SaveChangesAsync();
+            return IdentityResult.Success;
+
         }
 
-        public Task<Profile> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        public async Task<Profile> FindByIdAsync(string roleId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return await this.Context.Profiles.FirstOrDefaultAsync(x => x.Id == new Guid(roleId));
         }
 
-        public Task<Profile> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        public async Task<Profile> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return await this.Context.Profiles.FirstOrDefaultAsync(x => x.Name == normalizedRoleName);
         }
 
         public Task<string> GetNormalizedRoleNameAsync(Profile role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(role.Name);
         }
 
         public Task<string> GetRoleIdAsync(Profile role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(role.Id.ToString());
         }
 
         public Task<string> GetRoleNameAsync(Profile role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(role.Name);
         }
 
         public Task SetNormalizedRoleNameAsync(Profile role, string normalizedName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            role.Name = normalizedName;
+            return Task.CompletedTask;
         }
 
         public Task SetRoleNameAsync(Profile role, string roleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            role.Name = roleName;
+            return Task.CompletedTask;
         }
 
-        public Task<IdentityResult> UpdateAsync(Profile role, CancellationToken cancellationToken)
+        public async Task<IdentityResult> UpdateAsync(Profile role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var roleToUpdate = await this.Context.Profiles.AsNoTracking().FirstOrDefaultAsync(x => x.Id == role.Id);
+
+            roleToUpdate = role;
+            this.Context.Entry(roleToUpdate).State = EntityState.Modified;
+
+            this.Context.Profiles.Add(roleToUpdate);
+            await this.Context.SaveChangesAsync();
+
+            return IdentityResult.Success;
+
+
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~ProfileRepository()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
